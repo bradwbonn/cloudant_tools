3e1d00cc6f1f91c3885b90edc3ac0ace7cf8a89b
Brad Bonn <brad.bonn@cloudant.com>
2014-03-06 12:39:53 -0500

    fixed JSON validation output

--- csv_to_json_test.py
+++ csv_to_json_test.py
@@ -5,24 +5,27 @@ import json
 
 csvfile = open('file.csv', 'r')
 jsonfile = open('file.json', 'w')
-rowcounter = 0
 
+# Specifying the field names will import the first row as data but name the fields
+# Remove "fieldnames" from the DictReader statement to use the first line as the
+# field names themselves.
 fieldnames = ("FirstName","LastName","IDNumber","Message")
 reader = csv.DictReader( csvfile, fieldnames)
 
 # Output the beginning of the over-arching JSON doc that will contain an array of docs
-jsonfile.write('{ "docs:[')
+jsonfile.write('{ "docs":[')
+
+rowcount = 0
 
 for row in reader:
-    # If this isn't the first row, lead a comma
-    if reader.line_num == 1:
-        jsonfile.write('\n')
+    # If this isn't the first document to write, lead a comma
+    if rowcount != 0:
+        jsonfile.write(',')
     else:
-        jsonfile.write(',\n')
+        rowcount = rowcount + 1
     
-    # Output the current row as a JSON document and append a new line for readability
+    # Output the current row as a JSON document
     json.dump(row, jsonfile)
-    jsonfile.write('\n')
 
 # Write the final closing bracket for the "docs" document
-jsonfile.write(']\n}')
+jsonfile.write(']}')
