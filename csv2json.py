7c6ab6468e92f1c6e98bf641c7afa6b828bed93f
Brad Bonn <brad.bonn@cloudant.com>
2014-03-07 13:09:59 -0500

    Added number of rows per file function, yay!

--- csv2json.py
+++ csv2json.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-import csv
-import json
-import sys
-import getopt
-
+# csv2json.py
+# Brad Bonn
+# Cloudant, Inc.
+# brad.bonn@cloudant.com
+#
 # This script imports a csv file and converts it into a JSON document file(s) that contains
 # an array of JSON documents, each corresponding to one row of the CSV file.
 #
-# It can be paired with a /_bulk_docs API call to import a large number of docs
+# The output can be paired with a /_bulk_docs API call to import a large number of docs
 # into a database in chunks, rather than one at a time.
 #
 # The script accepts the following parameters:
@@ -21,8 +21,13 @@ import getopt
 #     causes '-o' to be ignored, and creates files with the name 'file_xxx.json'
 #     with 'xxx' incrementing)
 
+import csv
+import json
+import sys
+import getopt
+
 # Pre-defined help string to print upon '-h' or args mistake
-help = 'csv2json.py -i <inputfile> [-o <outputfile>] [-f <field names>] [-s] [-n]'
+help = 'csv2json.py -i <inputfile> [-o <outputfile>] [-f <field names>] [-s] [-n <rows per file>]'
 
 def main(argv):
     
@@ -117,12 +122,55 @@ def createsinglejsonfile(csvdict,outputfilename,dontusefirstrow):
     jsonfile.close()
     print 'Rows converted: ',rowcount
     pass
-
     
-def createjsonfiles(csvdict,rowcount,dontusefirstrow):
-    # If rowcount = 0, put everything into one file and ignore filenamestring
-    print "Sorry, this function doesn't exist just yet\n"
-    sys.exit()
+def createjsonfiles(csvdict,filelength,dontusefirstrow):
+    # For every row, find the state. States can be:
+    # 1) First row for a new file
+    # 2) Row within a file
+    # 3) Last row within a file
+    
+    rowcount = 1
+    skipcheck = 0
+    filecount = 0
+    
+    for row in csvdict:
+        currentfilename = 'file_%d.json' % filecount
+        # Go to next loop if we're in the first iteration and we want to skip the first row
+        if (skipcheck == 0 and dontusefirstrow == 1):
+            skipcheck = 1
+            continue
+        # If this is the first row for a sequence, open the new JSON doc, write the beginning,
+        # then output the row and increase rowcount
+        if (rowcount == 1):
+            jsonfile = open(currentfilename, 'w')
+            jsonfile.write('{ "docs":[')
+            json.dump(row, jsonfile)
+            rowcount = rowcount + 1
+        # If this is a row in the middle of a document, write a leading comma, then output
+        # the row and increase rowcount
+        elif ((rowcount > 1) and (int(rowcount) / int(filelength) != int(rowcount) / float(filelength))):
+            jsonfile.write(',')
+            rowcount = rowcount + 1
+            json.dump(row, jsonfile)
+        # If this is the last row for a document, output the comma, row, then write the final
+        # closing bracket, close the file, then reset rowcount to 1 and increment filecount
+        elif ((rowcount > 1) and (int(rowcount) / int(filelength) == int(rowcount) / float(filelength))):
+            jsonfile.write(',')
+            json.dump(row, jsonfile)
+            jsonfile.write(']}')
+            jsonfile.close()
+            rowcount = 1
+            filecount = filecount + 1
+        else:
+            print "That just don't add up! (row checking error)"
+            sys.exit(2)
+    # If the file handler is still open, append the final closing bracket and then close it
+    if (jsonfile.closed):
+        print 'Final file written: ' + currentfilename
+    else:
+        jsonfile.write(']}')
+        jsonfile.close()
+        print 'Final file written: ' + currentfilename
     pass
 
 if __name__ == "__main__":
