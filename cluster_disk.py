16304d5ed336c81cf2ba2bfd4a87ca7b44f6dd90
Brad Bonn <github@theotter.net>
2015-12-30 17:34:06 -0500

    Bug fixes
    1. nodes are now listed in numerical order
    2. data volume formatting now adapts to fit number of bytes
    automatically
    3. Code improvements and formatting tweaks

--- cluster_disk.py
+++ cluster_disk.py
@@ -43,18 +43,24 @@ def get_node_list(cluster):
         headers = my_header
     )
     json_response = r.json()
-    garbage1 = 'dbcore@'
+    garbage1 = 'dbcore@db'
     garbage2 = '.' + cluster + '.cloudant.net'
     for nodestring in json_response['cluster_nodes']:
         # regex out the node name
         without_tail = re.sub(garbage2,'',nodestring)
         nodename = re.sub(garbage1,'',without_tail)
         # add it to the list
-        nodes.append(nodename)
+        nodes.append(int(nodename))
     return(nodes)
     
 def get_disk_state_of_node(node, account, cluster):
-    myurl = 'https://' + account + '.cloudant.com/_api/v2/monitoring/node_disk_use_srv?cluster=' + cluster + '&format=json&node=' + node
+    urlformat = 'https://{0}.cloudant.com/_api/v2/monitoring/node_disk_{1}_srv?cluster={2}&format=json&node=db{3}'
+    myurl = urlformat.format(
+        account,
+        'use',
+        cluster,
+        node
+    )
     r = requests.get(
         myurl,
         headers = my_header
@@ -63,7 +69,12 @@ def get_disk_state_of_node(node, account, cluster):
         print r.status_code
         sys.exit("Cannot query node stats: " + node)
     disk_used = r.json()
-    myurl = 'https://' + account + '.cloudant.com/_api/v2/monitoring/node_disk_free_srv?cluster=' + cluster + '&format=json&node=' + node
+    myurl = urlformat.format(
+        account,
+        'free',
+        cluster,
+        node
+    )
     r = requests.get(
         myurl,
         headers = my_header
@@ -89,19 +100,20 @@ def print_results(cluster):
     print ""
     print "Disk usage on the "+ str(len(results)) +" nodes of cluster: " + cluster
     total_percent_change = 0
-    total_gb_used = 0
-    total_gb_free = 0
-    total_gb_previous = 0
+    total_disk_used = 0
+    total_disk_free = 0
+    total_disk_previous = 0
     timediffs = []
     for key in sorted(results):
-        change = int((results[key][1] - results[key][3]) / 1024 / 1024)
-        percent_change = round(((results[key][1] - results[key][3]) / results[key][3]) * 100, 1)
-        percent_full = round((results[key][1] / (results[key][0] + results[key][1])) * 100, 1)
-        gb_used = round(((results[key][1] / 1024) / 1024) / 1024, 0)
-        total_gb_previous = total_gb_previous + round(((results[key][3] / 1024) / 1024) / 1024, 0)
-        total_gb_used = total_gb_used + gb_used
-        total_gb_free = total_gb_free + round(((results[key][0] / 1024) / 1024) / 1024, 0)
-        timediff = (results[key][4] / 60)
+        result = results[key]
+        change = result[1] - result[3]
+        percent_change = round(((result[1] - result[3]) / result[3]) * 100, 1)
+        percent_full = round((result[1] / (result[0] + result[1])) * 100, 1)
+        disk_used = result[1]
+        total_disk_previous = total_disk_previous + result[3]
+        total_disk_used = total_disk_used + disk_used
+        total_disk_free = total_disk_free + result[0]
+        timediff = (result[4] / 60)
         timediffs.append(timediff)
         if percent_full > 90:
             tag = '*'
@@ -115,11 +127,21 @@ def print_results(cluster):
             plusornot = "-"
             percent_change = abs(percent_change)
             change = abs(change)
-        print '{0:5}- Used:{1:7}GB ({2:4}%){3} Change:{4}{5:5}MB ({6}{7:4}% in {8:1}min)'.format(key,gb_used,percent_full,tag,plusornot,change,plusornot,percent_change,timediff)
-    total_change = total_gb_used - total_gb_previous
-    total_percent_change = round(((total_gb_used - total_gb_previous) / total_gb_previous) * 100, 1)
+        print 'db{0:<3}:{1:>9} ({2:4}%){3} Change:{4}{5:>9} ({6}{7:4}% in {8:1}min)'.format(
+            key,
+            data_size_pretty(disk_used),
+            percent_full,
+            tag,
+            plusornot,
+            data_size_pretty(change),
+            plusornot,
+            percent_change,
+            timediff
+        )
+    total_change = total_disk_used - total_disk_previous
+    total_percent_change = round(((total_disk_used - total_disk_previous) / total_disk_previous) * 100, 1)
     total_timediff = int(round(numpy.mean(timediffs)))
-    total_percent_full = round((total_gb_used / (total_gb_free + total_gb_used)) * 100, 1)
+    total_percent_full = round((total_disk_used / (total_disk_free + total_disk_used)) * 100, 1)
     if total_change > 0:
         total_plusornot = "+"
     else:
@@ -127,9 +149,28 @@ def print_results(cluster):
         total_percent_change = abs(total_percent_change)
         total_change = abs(total_change)
     print ""
-    #print "TOTALS: Used:{0}GB ({1}%)  Change:({2}{3}GB {4}% in {5}min)".format(total_gb_used,total_percent_full,total_plusornot,total_change,total_percent_change,total_timediff)
-    print 'TOTAL: Used:{0:7}GB ({1:4}%)  Change:{2}{3:5}GB ({4}{5:4}% in {6:1}min)'.format(total_gb_used,total_percent_full,total_plusornot,total_change,total_plusornot,total_percent_change,total_timediff)
+    print 'TOTAL:{0:>9} ({1:4}%)  Change:{2}{3:>9} ({4}{5:4}% in {6:1}min)'.format(
+        data_size_pretty(total_disk_used),
+        total_percent_full,
+        total_plusornot,
+        data_size_pretty(total_change),
+        total_plusornot,
+        total_percent_change,
+        total_timediff
+    )
     print ""
+
+def data_size_pretty(size):
+    measure = 0
+    size = float(size)
+    while (size > 1024):
+        size = round(size / 1024, 2)
+        measure = measure + 1
+    codes = [' b',' K',' M',' G',' T',' P']
+    if measure == 0:
+        size = int(size)
+    formattedsize = "{:,}".format(size)
+    return (formattedsize + codes[measure])  
 
 if __name__ == "__main__":
     main(sys.argv[1:])
